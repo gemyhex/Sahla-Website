3eeb154f6183aa58671da2c48fc3e13da01fd758
J I M M Y <meshoinc2018@gmail.com>
2022-04-16 23:34:51 +0200

    sahla

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="d-flex flex-nowrap justify-space-between align-items-center">
+    <div class="logo">
+      <img src="../assets/images/logo.svg" alt="" width="150px" />
+    </div>
+    <div class="links">
+      <ul class="d-flex">
+        <li class="px-3" v-for="(link, i) in links" :key="i">
+          <router-link :to="link.to">{{ link.title }}</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="auth">
+      <v-btn color="#602167" elevation="2">{{ auth }}</v-btn>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { title: "Home", to: "/" },
+        { title: "About Us", to: "/aboutus" },
+        { title: "Offers", to: "/offers" },
+        { title: "Our Network", to: "/network" },
+        { title: "Jion as a Merchant", to: "/merchant" },
+        { title: "Contact Us", to: "/contactus" },
+      ],
+      auth: "Sign Up",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.theme--light.v-application {
+  .auth {
+    .v-btn {
+      color: #fff !important;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fluid class="home">
+    <v-row>
+      <v-col class="curve"> </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  min-height: 1000px;
+  .curve {
+    min-height: calc(100vh - 80px);
+    position: relative;
+    background: linear-gradient(90deg, #f0027f, #75489f);
+    &:before {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 100px;
+      background: url('../assets/images/border.svg');
+    }
   }
 }
-</script>
+</style>
